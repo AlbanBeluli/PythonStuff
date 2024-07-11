84d42093aa262aa8d82c9af36754255255126834
Alban Beluli <126069240+AlbanBeluli@users.noreply.github.com>
2024-07-11 11:07:48 +0200

    Create logic.py

---
+++ logic.py
@@ -0,0 +1,9 @@
+color = input("What is your fafourite color?")
+if color == "blue":
+    print("Love blue!")
+elif color == "pink":
+    print("Girls like that.")
+elif color == "orange":
+    print("I want to eat an orange.")
+else:
+    print("Okay.")
